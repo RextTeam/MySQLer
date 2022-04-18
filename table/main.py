66f8188b15525f6eb14a1beb7de21f13a21a558e
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-18 19:50:16 +0900

    Update main.py

--- table/main.py
+++ table/main.py
@@ -44,35 +44,3 @@ class Table(metaclass=TableMeta):
             text = " AND ".join(f"{name}=%s" for name in kwargs)
             data = [kwargs[name] for name in kwargs]
             return f"SELECT * FROM {self.tablename} WHERE {text}", data
-
-class TableManager:
-    def __init__(self, tablename, **kwargs):
-        self.tablename = tablename
-        self.kwargs = kwargs
-
-    @property
-    def create(self):
-        colum = {}
-        for name in self.kwargs:
-            nametype = self.kwargs[name]
-            if isinstance(str(), nametype):
-                colum[name] = "TEXT"
-            elif isinstance(int(), nametype):
-                colum[name] = "BIGINT"
-        last = ", ".join(f"{name} {colum[name]}"
-                         for name in colum)
-        return f"CREATE TABLE {self.tablename} " + last
-
-    def insert(self, **kwargs):
-        colums = ", ".join(name for name in kwargs)
-        values = [kwargs[name] for name in kwargs]
-        valuetext = ', '.join('%s' for i in values)
-        return f"INSERT INTO {self.tablename}({colums}) VALUES({valuetext})", values
-
-    def select(self, **kwargs):
-        if kwargs == {}:
-            return f"SELECT * FROM {self.tablename}"
-        else:
-            text = " AND ".join(f"{name}=%s" for name in kwargs)
-            data = [kwargs[name] for name in kwargs]
-            return f"SELECT * FROM {self.tablename} WHERE {text}", data
