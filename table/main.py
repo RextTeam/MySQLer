6461b83e6d1e9ce7360a9aab296397508b331a56
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-18 19:37:46 +0900

    Update main.py

--- table/main.py
+++ table/main.py
@@ -1,3 +1,50 @@
+class ColumnType:
+    name = None
+
+class StrColumn(ColumnType):
+    name = "TEXT"
+
+class IntColumn(ColumnType):
+    name = "BIGINT"
+    
+class TableMeta(type):
+    def __new__(cls, name, base, d, **kwargs):
+        d["tablename"] = kwargs.pop("table_name", name)
+        columns = {}
+        for name, type_ in d.items:
+            if isinstance(type_, ColumnType):
+                columns[name] = type_.name
+        d["columns"] = columns
+        return super().__new__(cls, name, base, d)
+    
+class TableError(Exception):
+    pass
+    
+class Table(metaclass=TableMeta):
+    def create(self):
+        columns = ', '.join(f'{i} {self.columns[i]}' for i in self.columns)
+        return f"CREATE TABLE {self.tablename}({columns})"
+    
+    def insert(self, **kwargs):
+        for name in kwargs:
+            if not name in self.columns:
+                raise TableError("存在しないカラムです")
+        colums = ", ".join(name for name in kwargs)
+        values = [kwargs[name] for name in kwargs]
+        valuetext = ', '.join('%s' for i in values)
+        return f"INSERT INTO {self.tablename}({colums}) VALUES({valuetext})", values
+    
+    def select(self, **kwargs):
+        if kwargs == {}:
+            return f"SELECT * FROM {self.tablename}"
+        else:
+            for name in kwargs:
+                if not name in self.columns:
+                    raise TableError("存在しないカラムです")
+            text = " AND ".join(f"{name}=%s" for name in kwargs)
+            data = [kwargs[name] for name in kwargs]
+            return f"SELECT * FROM {self.tablename} WHERE {text}", data
+
 class TableManager:
     def __init__(self, tablename, **kwargs):
         self.tablename = tablename
